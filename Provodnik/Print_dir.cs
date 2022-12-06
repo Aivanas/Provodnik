c5f1ea66046e3e0f3fb9ce625151814a169edd4c
Aivanas <vk53418006@gmail.com>
2022-12-06 22:48:23 +0300

    Initial commit

---
+++ Provodnik/Print_dir.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.IO;
+
+namespace Provodnik
+{
+    public class Print_dir
+    {
+        public static void ShowDirectory(string dir_path)
+        {
+            int choosed_line = 0;
+            while (true)
+            {
+                
+                
+                string[] allDirectories = Directory.GetDirectories(dir_path);
+                string[] allFiles = Directory.GetFiles(dir_path);
+                Console.WriteLine("----------------------------------------------------------" +
+                                  "-----------------------------------------------");
+                Console.WriteLine("| F1 - Создать новый файл | F2 - Создать новую директорию | " +
+                                  "F3 - Удалить файл | F4 - Удалить директорию |");
+                Console.WriteLine("----------------------------------------------------------" +
+                                  "-----------------------------------------------");
+                Console.WriteLine(dir_path);
+                Console.WriteLine("");
+            
+                foreach (string dir in allDirectories)
+                {
+                    if (choosed_line == Array.IndexOf(allDirectories, dir))
+                    {
+                        Console.WriteLine($"----> {dir}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"     {dir}");
+                    }
+                }
+
+                foreach (string file in allFiles)
+                {
+                    if (choosed_line == (Array.IndexOf(allFiles, file) + allDirectories.Length))
+                    {
+                        Console.WriteLine($"----> {file}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"     {file}");
+                    }
+                }
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow & choosed_line > 0)
+                {
+                    choosed_line--;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.DownArrow & choosed_line < (allDirectories.Length + allFiles.Length - 1))
+                {
+                    choosed_line++;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    if (choosed_line <= allDirectories.Length)
+                    {
+                        Print_dir.ShowDirectory(allDirectories[choosed_line]);
+                    }
+                    
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    Console.Clear();
+                    break;
+
+                }
+                else if (key.Key == ConsoleKey.F1)
+                {
+                    Console.Clear();
+                    Console.Write("Введите название файла: ");
+                    string FileName = Console.ReadLine();
+                    File.Create(dir_path + "\\" + FileName);
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.F2)
+                {
+                    Console.Clear();
+                    Console.Write("Введите название директории: ");
+                    string DirName = Console.ReadLine();
+                    Directory.CreateDirectory(dir_path + "\\" + DirName);
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.F3)
+                {
+                    Console.Clear();
+                    int DeletingChoosedLine = 0;
+                    while (true)
+                    {
+                        Console.WriteLine($"Вы уверены что хотите удалить файл {allFiles[choosed_line - allDirectories.Length]}?");
+                        if (DeletingChoosedLine == 0)
+                        {
+                            Console.WriteLine("---> Нет");
+                            Console.WriteLine("     Да");
+                        }
+                        else
+                        {
+                            Console.WriteLine("     Нет");
+                            Console.WriteLine("---> Да");
+                        }
+
+                        ConsoleKeyInfo deleting_key = Console.ReadKey();
+
+                        if (deleting_key.Key == ConsoleKey.UpArrow & DeletingChoosedLine > 0)
+                        {
+                            DeletingChoosedLine--;
+                            Console.Clear();
+                        }
+                        else if (deleting_key.Key == ConsoleKey.DownArrow & DeletingChoosedLine < 1)
+                        {
+                            DeletingChoosedLine++;
+                            Console.Clear();
+                        }
+                        else if (deleting_key.Key == ConsoleKey.Enter)
+                        {
+                            if (DeletingChoosedLine == 0)
+                            {
+                                Console.Clear();
+                                break;
+                            }
+                            else
+                            {
+                                File.Delete(allFiles[choosed_line - allDirectories.Length]);
+                                Console.Clear();
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.Clear();
+                        }
+
+                    }
+                }
+                else if (key.Key == ConsoleKey.F4)
+                {
+                    Console.Clear();
+                    int DeletingChoosedLine = 0;
+                    while (true)
+                    {
+                        Console.WriteLine($"Вы уверены что хотите удалить директорию {allDirectories[choosed_line]}?");
+                        if (DeletingChoosedLine == 0)
+                        {
+                            Console.WriteLine("---> Нет");
+                            Console.WriteLine("     Да");
+                        }
+                        else
+                        {
+                            Console.WriteLine("     Нет");
+                            Console.WriteLine("---> Да");
+                        }
+
+                        ConsoleKeyInfo deleting_key = Console.ReadKey();
+
+                        if (deleting_key.Key == ConsoleKey.UpArrow & DeletingChoosedLine > 0)
+                        {
+                            DeletingChoosedLine--;
+                            Console.Clear();
+                        }
+                        else if (deleting_key.Key == ConsoleKey.DownArrow & DeletingChoosedLine < 1)
+                        {
+                            DeletingChoosedLine++;
+                            Console.Clear();
+                        }
+                        else if (deleting_key.Key == ConsoleKey.Enter)
+                        {
+                            if (DeletingChoosedLine == 0)
+                            {
+                                Console.Clear();
+                                break;
+                            }
+                            else
+                            {
+                                DeleteDirectory(allDirectories[choosed_line]);
+                                Directory.Delete(allDirectories[choosed_line]);
+                                Console.Clear();
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.Clear();
+                        }
+                    }
+                }
+                
+                
+                
+                else
+                {
+                    Console.Clear();
+                }
+                
+            }
+            
+
+        }
+
+        public static void DeleteDirectory(string dir_path)
+        {
+            string[] allDirectories = Directory.GetDirectories(dir_path);
+            string[] allFiles = Directory.GetFiles(dir_path);
+
+            foreach (var file in allFiles)
+            {
+                File.Delete(file);
+            }
+
+            foreach (var directory in allDirectories)
+            {
+                DeleteDirectory(directory);
+                Directory.Delete(directory);
+            }
+        }
+    }
+}
---
+++ Provodnik/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
+
+namespace Provodnik
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            int choosed_line = 1;
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            while (true)
+            {
+                int i = 1;
+                
+                foreach (DriveInfo drive in drives)
+                {
+                    if (i == choosed_line)
+                    {
+                        Console.WriteLine(
+                            $"---> {i}. Диск {drive.Name} Свободно {drive.AvailableFreeSpace / 1073741824} " +
+                            $"Гб из {drive.TotalSize / 1073741824} Гб");
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"    {i}. Диск {drive.Name} Свободно {drive.AvailableFreeSpace / 1073741824} " +
+                            $"Гб из {drive.TotalSize / 1073741824} Гб");
+                    }
+                    i++;
+                }
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow & choosed_line > 1)
+                {
+                    choosed_line--;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.DownArrow & choosed_line < drives.Length)
+                {
+                    choosed_line++;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Print_dir.ShowDirectory(drives[choosed_line - 1].Name);
+                    
+                }
+                else
+                {
+                    Console.Clear();
+                }
+                
+            }
+            
+            
+
+            
+            
+        }
+    }
+}
